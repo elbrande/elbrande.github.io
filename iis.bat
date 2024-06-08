fbc78b65925e54317df148236635d64885ed2f2a
Eric Brandenburg <elbrande@gmail.com>
2024-06-08 13:58:55 -0400

    census

---
+++ iis.bat
@@ -0,0 +1,4 @@
+"\Program Files (x86)\IIS Express\iisexpress" /path:"C:\Users\elbrande\Documents\GitHub\elbrande.github.io" /port:9192
+
+rem npm install -g iisexpress-proxy
+rem iisexpress-proxy 9090 to 81
